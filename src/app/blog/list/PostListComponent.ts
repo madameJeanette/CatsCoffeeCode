cacea240c2d29a40d3a0f6afa85c278431447e62
danie <danie@DESKTOP-M2H76EP>
2019-08-16 01:45:38 +0200

    cards now have dynamic info

--- src/app/blog/list/PostListComponent.ts
+++ src/app/blog/list/PostListComponent.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PostListComponent implements OnInit {
+    public postList = postList;
     constructor() { }
 
     ngOnInit() { }
 }
+
+const postList = [
+    {
+        title: 'Very cool title',
+        subTitle: 'such amazing sub title',
+        imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+        content:" The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+    {
+        title: 'Such wow',
+        subTitle: 'much amaze',
+        imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+        content:" The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+    {
+        title: 'Nicu',
+        subTitle: 'desune',
+        imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+        content:" The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+    {
+        title: 'Bork',
+        subTitle: 'bork',
+        imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+        content:" The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    }
+];
--- src/app/blog/list/PostListModule.ts
+++ src/app/blog/list/PostListModule.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {PostListComponent} from './PostListComponent' ;
 import {MatButtonModule, MatCardModule} from '@angular/material'; 
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
+        CommonModule,
         MatCardModule,
         MatButtonModule
     ],
